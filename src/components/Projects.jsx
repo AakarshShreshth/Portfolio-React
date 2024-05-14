4974ec900497b533289ac1557d42536dae6d08a1
Aakarsh Shreshth <aakarshshreshth@gmail.com>
2024-05-14 12:23:05 +0530

    Update images links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div id="projects" className='max-w-[1040px] m-auto md:pl-28 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e] mb-10'>Projects</h1>
       <div className='grid sm:grid-cols-2 gap-12'>
-        <ProjectItem img={projectImg} title='Portfolio' stack='ReactJs'/>
+        <ProjectItem img={projectImg} title='Portfolio' stack='ReactJs' gh='https://github.com/AakarshShreshth/Portfolio-React'/>
         <ProjectItem img={projectImg} title='Ticket Show' stack='Flask' gh='https://github.com/AakarshShreshth/Ticket-Show'/>
         <ProjectItem img={projectImg} title='Water Quality Prediction' stack='Scikit-Learn' gh='https://github.com/AakarshShreshth/Water-Quality-Prediction'/>
         <ProjectItem img={projectImg} title='Blog Lite' stack='Flask' gh='https://github.com/AakarshShreshth/Blog-Lite'/>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,51 +1,51 @@
 const skills = [
     {
         "title": "HTML",
-        "src": "src/assets/html-5.png"
+        "src": "/src/assets/html-5.png"
     },
     {
         "title": "CSS",
-        "src": "src/assets/css-3.png"
+        "src": "/src/assets/css-3.png"
     },
     {
         "title": "JS",
-        "src": "src/assets/js.png"
+        "src": "/src/assets/js.png"
     },
     {
         "title": "C++",
-        "src": "src/assets/c-.png"
+        "src": "/src/assets/c-.png"
     },
     {
         "title": "Python",
-        "src": "src/assets/python.png"
+        "src": "/src/assets/python.png"
     },
     {
         "title": "Java",
-        "src": "src/assets/java.png"
+        "src": "/src/assets/java.png"
     },
     {
         "title": "React",
-        "src": "src/assets/react.png"
+        "src": "/src/assets/react.png"
     },
     {
         "title": "NodeJs",
-        "src": "src/assets/nodejs.png"
+        "src": "/src/assets/nodejs.png"
     },
     {
         "title": "Git",
-        "src": "src/assets/git.svg"
+        "src": "/src/assets/git.svg"
     },
     {
         "title": "GitHub",
-        "src": "src/assets/github.png"
+        "src": "/src/assets/github.png"
     },
     {
         "title": "Linux",
-        "src": "src/assets/linux.png"
+        "src": "/src/assets/linux.png"
     },
     {
         "title": "MongoDB",
-        "src": "src/assets/mongodb.svg"
+        "src": "/src/assets/mongodb.svg"
     },
 ]
 
